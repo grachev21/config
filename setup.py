180c7603c5008206588bf0103b03f51ba3fc95c6
grachev <grachev@grachev.grachev>
2023-09-01 07:43:56 +0400

    update

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import subprocess
 
 list_command = [
+        'sudo apt install xorg lightdm slick-greeter lightdm-settings pulseaudio alsa-utils pavucontrol terminator curl i3status i3 lxappearance nitrogen vim git ranger rofi gpick arc-theme fonts-font-awesome, font-manager',
         'touch ~/.token',
-        'mkdir ~/.config/i3',
-        'cp ~/configi3/config ~/.config/i3/config',
-        'mkdir ~/.config/i3status',
-        'cp ~/config/i3status/config ~/.config/i3status/config',
-        'cp ~/config/.bash_aliases ~/.bash_aliases',
-        'cp ~/config/.vimrc ~/.vimrc',
+        #'mkdir ~/.config/i3',
+        'cp ./i3/config ~/.config/i3/config',
+        #'mkdir ~/.config/i3status',
+        'cp ./i3status/config ~/.config/i3status/config',
+        'cp ./.bash_aliases ~/.bash_aliases',
+        'cp ./.vimrc ~/.vimrc',
         'curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz',
         'curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
-        'sudo apt xorg lightdm slick-greeter lightdm-settings pulseaudio alsa-utils pavucontrol terminator install i3status i3 lxappearance nitrogen vim git ranger rofi gpick arc-theme fonts-font-awesome, font-manager'
         ]
 for command in list_command:
     subprocess.call(command, shell=True)
